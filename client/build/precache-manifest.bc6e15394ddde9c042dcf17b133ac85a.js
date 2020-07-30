9e77536d579b1e73268b883aa741ddb6b0798b51
ranjitha-s <ranjitha210497@gmail.com>
2020-07-30 14:52:42 +0530

    sl

---
+++ client/build/precache-manifest.bc6e15394ddde9c042dcf17b133ac85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe428a01f665ed3ad5effd73515f2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd8ab83af46b9ebe65d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe5c2bf52d7dd6d4cc59",
+    "url": "/static/css/main.bb3f5909.chunk.css"
+  },
+  {
+    "revision": "fcd8ab83af46b9ebe65d",
+    "url": "/static/js/2.527df843.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.527df843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5c2bf52d7dd6d4cc59",
+    "url": "/static/js/main.e11f2b7e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ranjitha:1deepmotu@cluster0.felrw.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+};
